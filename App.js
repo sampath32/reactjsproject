973c4ac50402ac87758e229d87eed05f4552b54f
sampath32 <31746162+sampath32@users.noreply.github.com>
2017-09-08 16:21:34 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+
+const App = () => (
+  <MuiThemeProvider>
+    <MyAwesomeReactComponent />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
+export default App;
---
+++ MyAwesomeReactComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const MyAwesomeReactComponent = () => (
+  <RaisedButton label="Click ME!!" />
+);
+
+export default MyAwesomeReactComponent;
